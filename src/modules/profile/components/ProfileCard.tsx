f5b1b21bea1a5daf7027a62a892fd9d0c67ee988
abrapidinnovation <shaileshkala1993@gmail.com>
2022-04-07 17:11:50 +0530

    component setup

---
+++ src/modules/profile/components/ProfileCard.tsx
@@ -0,0 +1,10 @@
+import { withTheme, ThemeProps } from 'styled-components'
+export const ProfileCard: React.FC = withTheme((props: ThemeProps<any>) => {
+    const { theme } = props
+
+    return (
+        <div>
+         
+        </div>
+    )
+})
